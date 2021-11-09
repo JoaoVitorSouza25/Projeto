39ea9548332c5c7572440fd77cb9077da846b6a3
João Vitor de Paula Souza <contato.joaovps@gmail.com>
2021-11-09 10:50:45 -0300

    Initial commit

---
+++ src/main/java/Controller/ConexaoTest.java
@@ -0,0 +1,20 @@
+
+package Controller;
+
+import java.sql.Connection;
+
+public class ConexaoTest {
+    
+    public static void main(String[] args) {
+        Connection con = Conexao.conectar();
+        
+        if(con != null)
+        {
+            System.out.println("Conexão realizada com sucesso");
+            
+            Conexao.desconectar(con);
+        }
+    }
+    
+    
+}
